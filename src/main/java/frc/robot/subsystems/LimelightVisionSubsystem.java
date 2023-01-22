d132fb6ad78e919a5e8e9719cc6e9afcacc508c7
maxhv1 <max.hero.factory@gmail.com>
2023-01-22 14:02:26 -0500

    limelight subsystem raw data had, but no commands

--- src/main/java/frc/robot/subsystems/LimelightVisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightVisionSubsystem.java
@@ -22,17 +22,20 @@ public class LimelightVisionSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    //botpose is an array of 6 doubles, translation xyz then rotation xyz
     bp=NetworkTableInstance.getDefault().getTable("limelight").getEntry("botpose").getDoubleArray(new Double[] {});
-    if (bp.length>0){
-      System.out.println(bp[0]+","+bp[1]+","+bp[2]+","+bp[3]+","+bp[4]+","+bp[5]);
+    // if (bp.length>0){
+    //   System.out.println(bp[0]+","+bp[1]+","+bp[2]+","+bp[3]+","+bp[4]+","+bp[5]);
     
     updateWidgets();
     }
   }
 
+  
   private final ShuffleboardTab mainTab=Shuffleboard.getTab("Main");
   private final GenericPublisher mainBotPose=mainTab.add("botpose","").getEntry();
   private void updateWidgets(){
+    if(bp.length>0)
     mainBotPose.setString(bp[0]+","+bp[1]+","+bp[2]+","+bp[3]+","+bp[4]+","+bp[5]);
   }
 }
