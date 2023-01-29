3ffa798a3974f2fb38a7e149bc5e3f4439cdac3c
maxhv1 <max.hero.factory@gmail.com>
2023-01-29 15:33:55 -0500

    both IK solvers in place

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -16,11 +16,20 @@ public class Arm extends SubsystemBase {
   public void periodic() {
     // This method will be called once per scheduler run
   }
-
-  public void ArmIKDrive(double targetX, double targetY){//where x and y are relative to shoulder position
+  //prerequisites: view the robot so that the arm extends to the right
+    //when all arm angles are zeroed, the arm sticks straight out to the right
+    //positive theta is counter clockwise, negative theta is clockwise
+  public void ArmIKDrive(double targetX, double targetY,boolean stowable){//where x and y are relative to shoulder position //stow means it will stow nicely so by default TRUE
     double r=Math.sqrt(squareOf(targetY)+squareOf(targetX));
-    //might need to add/subtract 180 + offset from encoder
-    ArmUpperAngleTarget=Math.toDegrees(Math.acos((squareOf(r)+squareOf(kArmUpperLength)-squareOf(kArmLowerLength))/(2*kArmUpperLength*r))+Math.atan(targetY/targetX));
-    ArmLowerAngleTarget=Math.toDegrees(Math.acos((squareOf(kArmUpperLength)+squareOf(kArmLowerLength)-squareOf(r))/(2*kArmLowerLength*kArmUpperLength)));
+    double phi1=Math.acos((squareOf(kArmUpperLength)+squareOf(kArmLowerLength)-squareOf(r))/(2*kArmUpperLength*kArmLowerLength) );
+    double phi2=Math.asin(kArmLowerLength*Math.sin(phi1)/r);
+    //double phi3=Math.PI-(phi1+phi2);
+      if(!stowable){
+        ArmUpperAngleTarget=Math.toDegrees(phi1-Math.atan(targetY/targetX));
+        ArmLowerAngleTarget=Math.toDegrees(phi2-Math.PI);
+      } else{
+        ArmUpperAngleTarget=-Math.toDegrees(phi1-Math.atan(targetY/targetX));
+        ArmLowerAngleTarget=-Math.toDegrees(phi2-Math.PI);
+      }
   }
 }
