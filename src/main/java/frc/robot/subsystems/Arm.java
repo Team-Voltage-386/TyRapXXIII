e164cd63f79a2489c46c9a7f8fc2a59eadb82d38
maxhv1 <max.hero.factory@gmail.com>
2023-01-30 08:52:32 -0500

    Added some geometry precaution and alt. method

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -16,20 +16,28 @@ public class Arm extends SubsystemBase {
   public void periodic() {
     // This method will be called once per scheduler run
   }
+  
   //prerequisites: view the robot so that the arm extends to the right
     //when all arm angles are zeroed, the arm sticks straight out to the right
     //positive theta is counter clockwise, negative theta is clockwise
   public void ArmIKDrive(double targetX, double targetY,boolean stowable){//where x and y are relative to shoulder position //stow means it will stow nicely so by default TRUE
     double r=Math.sqrt(squareOf(targetY)+squareOf(targetX));
-    double phi1=Math.acos((squareOf(kArmUpperLength)+squareOf(kArmLowerLength)-squareOf(r))/(2*kArmUpperLength*kArmLowerLength) );
-    double phi2=Math.asin(kArmLowerLength*Math.sin(phi1)/r);
-    //double phi3=Math.PI-(phi1+phi2);
-      if(!stowable){
-        ArmUpperAngleTarget=Math.toDegrees(phi1-Math.atan(targetY/targetX));
-        ArmLowerAngleTarget=Math.toDegrees(phi2-Math.PI);
-      } else{
-        ArmUpperAngleTarget=-Math.toDegrees(phi1-Math.atan(targetY/targetX));
-        ArmLowerAngleTarget=-Math.toDegrees(phi2-Math.PI);
-      }
+    if(r<kArmLowerLength+kArmUpperLength&&r>Math.abs(kArmLowerLength-kArmUpperLength)){//check for geometrically possible triangle
+      double phi1=Math.acos((squareOf(kArmUpperLength)+squareOf(kArmLowerLength)-squareOf(r))/(2*kArmUpperLength*kArmLowerLength) );
+      double phi2=Math.asin(kArmLowerLength*Math.sin(phi1)/r);
+      //double phi3=Math.PI-(phi1+phi2);
+        if(!stowable){
+          ArmUpperAngleTarget=Math.toDegrees(phi1-Math.atan(targetY/targetX));
+          ArmLowerAngleTarget=Math.toDegrees(phi2-Math.PI);
+        } else{
+          ArmUpperAngleTarget=-Math.toDegrees(phi1-Math.atan(targetY/targetX));
+          ArmLowerAngleTarget=-Math.toDegrees(phi2-Math.PI);
+        }
+    }
+  }
+
+  //the default mode
+  public void ArmIKDrive(double targetX, double targetY){
+    ArmIKDrive(targetX, targetY, true);
   }
 }
