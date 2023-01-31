f1b9989820e661db2d7638da5e784772d13f36c9
LucasCodee <lucascornejo222@gmail.com>
2023-01-31 18:39:30 -0500

    Update Balance.java

--- src/main/java/frc/robot/commands/Autonomous/Balance.java
+++ src/main/java/frc/robot/commands/Autonomous/Balance.java
@@ -4,33 +4,37 @@ import com.ctre.phoenix.sensors.Pigeon2;
 import static frc.robot.Constants.DriveConstants.*;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Tracer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.utils.PID;
 
 public class Balance extends CommandBase {
     private double balanceTarget = 2.5;
-    private double robotBalance = 0;
+    private final Drivetrain DT;
     private double ypr[] = new double[3];
     public Pigeon2 pigeonIMU = new Pigeon2(kIMUid);
-    private PIDController pidcont = new PIDController(kMaxDriveSpeed, kIMUid, kIMUid);
+    private PID pid = new PID(0, 0, 0);
 
-    public Balance() {
-        
+    public Balance(Drivetrain dt) {
+        DT = dt;
     }
 
     @Override
     public void initialize() {
         System.out.println("Balance Starting");
-        //makes all wheels face the same direction (direction of target pos is pointed when called)
-        LeftFront.resetWheel();
-        RightFront.resetWheel();
-        LeftRear.resetWheel();
-        RightFront.resetWheel();
+        
+        // //makes all wheels face the same direction (direction of target pos is pointed when called)
+        // LeftFront.resetWheel();
+        // RightFront.resetWheel();
+        // LeftRear.resetWheel();
+        // RightFront.resetWheel();
 
-        //stop wheels from turning
-        LeftFront.steerMotor.set(0);
-        RightFront.steerMotor.set(0);
-        LeftRear.steerMotor.set(0);
-        RightRear.steerMotor.set(0);
+        // //stop wheels from turning
+        // LeftFront.steerMotor.set(0);
+        // RightFront.steerMotor.set(0);
+        // LeftRear.steerMotor.set(0);
+        // RightRear.steerMotor.set(0);
     }
 
     //maybe this'll work?
@@ -38,20 +42,15 @@ public class Balance extends CommandBase {
     public void execute() {
         //assigns ypr vals
         ypr[1] = pigeonIMU.getPitch();
-        //easier to read
-        robotBalance = ypr[1];
         pigeonIMU.getYawPitchRoll(ypr);
         //balancing
-        LeftFront.driveMotor.set(pidcont.calculate(balanceTarget - ypr[1]));
-        RightFront.driveMotor.set(pidcont.calculate(balanceTarget - ypr[1]));
-        LeftRear.driveMotor.set(pidcont.calculate(balanceTarget - ypr[1]));
-        RightRear.driveMotor.set(pidcont.calculate(balanceTarget - ypr[1]));
+        DT.xDriveTarget = pid.calc(balanceTarget - ypr[1]);
     }
 
     @Override
     public boolean isFinished() {
         //if the angle is straighter than the tolerance, it kills the command
-        if(Math.abs(robotBalance) < balanceTarget) {
+        if(Math.abs(ypr[1]) < balanceTarget) {
             end(true);
             return true;
         }
