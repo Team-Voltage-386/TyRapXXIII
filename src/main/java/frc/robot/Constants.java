f8a2db4a6510cc5bf3e171d052cd971c54694137
maxhv1 <max.hero.factory@gmail.com>
2023-02-07 20:47:15 -0500

    arnRange

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -76,7 +76,7 @@ public final class Constants {
     public static final double[] kArmLowerPID={.01,0.0,0.0};
 
     public static final double[] kShoulderSafezone={-90,0};//lower limit is index 0, upper limit is index 1
-    public static final double[] kElbowSafezone={0,90};//lowe limit is index 0, upper limit is index 1
+    public static final double[] kElbowSafezone={-45,90};//lowe limit is index 0, upper limit is index 1
     public static final double kArmMotorDeadband=.1;
     public static final double kShoulderMaxPercent=.5;
     public static final double kElbowMaxPercent=.3;
