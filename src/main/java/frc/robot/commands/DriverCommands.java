c8833ac1ac27bbc169e415a66e52244471ecb285
maxhv1 <max.hero.factory@gmail.com>
2023-02-13 19:55:03 -0500

    comment

--- src/main/java/frc/robot/commands/DriverCommands.java
+++ src/main/java/frc/robot/commands/DriverCommands.java
@@ -19,11 +19,11 @@ import static frc.robot.Constants.Limelightconstants.*;
 public class DriverCommands extends CommandBase {
 
     private Drivetrain driveTrain;
-    
+
     private Limelight limelight;
-    private apriltag target=null;
+    private apriltag target = null;
     private double facingtoscore;
-    private double deadband=0.1;
+    private double deadband = 0.1;
     private double adjustableXDist;
     private PID autoRPID;
     private PID autoXPID;
@@ -34,11 +34,11 @@ public class DriverCommands extends CommandBase {
         limelight = LL;
         addRequirements(driveTrain);
         addRequirements(limelight);
-        facingtoscore=-180;//set to 0 or 180 based on team
-        adjustableXDist=6.25;//-6.25 if on blue team
-        autoRPID=new PID(kAutoRotationPID[0],kAutoRotationPID[1],kAutoRotationPID[2]);
-        autoXPID=new PID(kAutoDriveXPID[0],kAutoDriveXPID[1],kAutoDriveXPID[2]);
-        autoYPID=new PID(kAutoDriveYPID[0],kAutoDriveYPID[1],kAutoDriveYPID[2]);
+        facingtoscore = -180;// set to 0 or 180 based on team
+        adjustableXDist = 6.25;// -6.25 if on blue team
+        autoRPID = new PID(kAutoRotationPID[0], kAutoRotationPID[1], kAutoRotationPID[2]);
+        autoXPID = new PID(kAutoDriveXPID[0], kAutoDriveXPID[1], kAutoDriveXPID[2]);
+        autoYPID = new PID(kAutoDriveYPID[0], kAutoDriveYPID[1], kAutoDriveYPID[2]);
     }
 
     @Override
@@ -50,60 +50,103 @@ public class DriverCommands extends CommandBase {
 
     @Override
     public void execute() {
-        HumanDriverControl=Math.abs(kDriver.getRawAxis(kLeftTrigger))<deadband;
-        driveTrain.xDriveTarget = -kDriver.getRawAxis(kLeftVertical) * kMaxDriveSpeed;
-        driveTrain.yDriveTarget = kDriver.getRawAxis(kLeftHorizontal) * kMaxDriveSpeed;
-        driveTrain.rotationTarget = -Math.pow(kDriver.getRawAxis(kRightHorizontal),3) * kMaxRotSpeed;
+        // set the flag for human control; currently the only time it is not human
+        // control is when the trigger is pressed
+        // when the trigger is pressed, right now it enters the primitive target lock on
+        // with the apritag/retroreflective
+        // humans can only drive with joystick while human driver control is on
+        HumanDriverControl = Math.abs(kDriver.getRawAxis(kLeftTrigger)) < deadband;
 
-        if(HumanDriverControl){
+        if (HumanDriverControl) {
+            // joystick input
             driveTrain.yDriveTarget = kDriver.getRawAxis(kLeftHorizontal) * kMaxDriveSpeed;
-            driveTrain.xDriveTarget = kDriver.getRawAxis(kLeftVertical) * kMaxDriveSpeed;
-            driveTrain.rotationTarget = -kDriver.getRawAxis(kRightHorizontal) * kMaxRotSpeed;
+            driveTrain.xDriveTarget = -kDriver.getRawAxis(kLeftVertical) * kMaxDriveSpeed;
+            driveTrain.rotationTarget = -Math.pow(kDriver.getRawAxis(kRightHorizontal), 3) * kMaxRotSpeed;
 
+            // this was the feature me and carl talked about a while ago; the robot
+            // typically pivots about its center
+            // BUT if we push the left bumper, the pivot point is offset
             if (kDriver.getRawButtonPressed(kLeftBumper))
                 driveTrain.setOffset(-0.65, 0);
             else if (kDriver.getRawButtonReleased(kLeftBumper))
                 driveTrain.setOffset(0, 0);
-
-            if (kDriver.getRawButtonPressed(kRightBumper)){
-                if(limelight.apriltagmode()&&limelight.apriltagsAvailable())
+            // reset the odometry
+            // will use apriltag if available to get proper field orientation;
+            // if no apriltags: set current orientation as zero (TBD; unsure what it thinks
+            // it is facing, but field oriented joystick is likely to be wrong)
+            if (kDriver.getRawButtonPressed(kRightBumper)) {
+                if (limelight.apriltagmode() && limelight.apriltagsAvailable())
                     driveTrain.setFO(limelightYawToDriveTrainYaw());
                 else
                     driveTrain.resetFO();
             }
         }
 
-        //align to apriltag
-        if(limelight.apriltagmode() && Math.abs(kDriver.getRawAxis(kLeftTrigger))>deadband&&limelight.apriltagsAvailable()){
-            HumanDriverControl=false;
-            if(target==null) {target=closestGrid(limelight.getPose()[0],limelight.getPose()[1]);}
+        // align to apriltag when Left trigger is pressed and in ApriltagPipeline
+        // override human control
+        if (limelight.apriltagmode() && Math.abs(kDriver.getRawAxis(kLeftTrigger)) > deadband
+                && limelight.apriltagsAvailable()) {
+            HumanDriverControl = false;
+            // target is null means we do not know which apriltag is closest yet
+            // use apriltag to find current position on field, then find closest grid on the
+            // field, then set target to closest grid
+            if (target == null) {
+
+                target = closestGrid(limelight.getPose()[0], limelight.getPose()[1]);// closest grid method is in
+                                                                                     // constants
+            }
             driveTrain.setFO(limelightYawToDriveTrainYaw());
-            driveToTarget(adjustableXDist,target.y,facingtoscore,limelight.getPose()[0],limelight.getPose()[1],driveTrain.getProcessedHeading());
-        } 
-        //align to retroreflective
-        if(limelight.retroreflectivemode() && Math.abs(kDriver.getRawAxis(kLeftTrigger))>deadband){
-            HumanDriverControl=false;
-            driveToTarget(0, 0, facingtoscore, 0, 1.5*Math.atan(Math.toRadians(limelight.tx())) , driveTrain.getProcessedHeading());//the value tiimes arctan should be a constant (variable of sorts)
-        } 
-        if(kDriver.getRawButtonPressed(kX)){//switch pipelines
-            if(limelight.apriltagmode())limelight.setPipeline(retroreflectivepipelineindex);
-            else if(limelight.retroreflectivemode())limelight.setPipeline(apriltagpipelineindex);
+            driveToTarget(adjustableXDist, target.y, facingtoscore, limelight.getPose()[0], limelight.getPose()[1],
+                    driveTrain.getProcessedHeading());
+        }
+        // align to retroreflective when Left trigger is pressed and in Retroreflective
+        // pipleine
+        if (limelight.retroreflectivemode() && Math.abs(kDriver.getRawAxis(kLeftTrigger)) > deadband) {
+            HumanDriverControl = false;
+            driveToTarget(0, 0, facingtoscore, 0, 1.5 * Math.atan(Math.toRadians(limelight.tx())),
+                    driveTrain.getProcessedHeading());
+            // the value 1.5 times arctan should be a constant TBD
+            // driving based off of target angles instead of robot position
+            // x is assumed to be correct, adjust only y and keep orientation good
+        }
+        //switch the pipelines
+        if (kDriver.getRawButtonPressed(kX)) {
+            if (limelight.apriltagmode())
+                limelight.setPipeline(retroreflectivepipelineindex);
+            else if (limelight.retroreflectivemode())
+                limelight.setPipeline(apriltagpipelineindex);
         }
     }
-    //feed limelight yaw into drivetrainyaw
-    public double limelightYawToDriveTrainYaw(){
-        return (limelight.getPose()[5])+180-90;//limelight field coordinate system is 180, gyroscope weirdness says subtract 90
+
+    /** feed limelight yaw into drivetrainyaw */ 
+    public double limelightYawToDriveTrainYaw() {
+        return (limelight.getPose()[5]) + 180 - 90;// limelight field coordinate system is 180, gyroscope weirdness says
+                                                   // subtract 90
     }
+
     @Override
     public boolean isFinished() {
         return false;
     }
-    //drive to target, feed in target position and rotation and robot position and rotation
-    public void driveToTarget(double targetX, double targetY, double targetRot, double bpx, double bpy, double bpr){
-        driveTrain.rotationTarget=autoRPID.calc(targetRot-bpr);
-        driveTrain.xDriveTarget=autoXPID.calc(targetX-bpx);
-        driveTrain.yDriveTarget=autoYPID.calc(targetY-bpy);
+
+    //
+    /**
+     * PID drive to target, feed in target position and rotation and robot position
+     * and rotation
+     * 
+     * @param targetX
+     * @param targetY
+     * @param targetRot
+     * @param bpx       bot pose x
+     * @param bpy       bot pose y
+     * @param bpr       bot pose rotation
+     */
+    public void driveToTarget(double targetX, double targetY, double targetRot, double bpx, double bpy, double bpr) {
+        driveTrain.rotationTarget = autoRPID.calc(targetRot - bpr);
+        driveTrain.xDriveTarget = autoXPID.calc(targetX - bpx);
+        driveTrain.yDriveTarget = autoYPID.calc(targetY - bpy);
     }
+
     @Override
     public void end(boolean interrupted) {
         driveTrain.yDriveTarget = 0;
--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -13,68 +13,88 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import static frc.robot.Constants.Limelightconstants.*;
 
-
 public class Limelight extends SubsystemBase {
   private Double[] bp;
-  private Double tx,ty;
+  private Double tx, ty;
   private boolean apriltagmode;
   private boolean retroreflectivemode;
   private NetworkTable nt;
+
   /** Creates a new LimelightVisionSubsystem. */
   public Limelight() {
-    nt=NetworkTableInstance.getDefault().getTable("limelight");
+    nt = NetworkTableInstance.getDefault().getTable("limelight");
   }
-  /**alternate constructor for Limelight*/
-  public Limelight(String networkTableName){
-    nt=NetworkTableInstance.getDefault().getTable(networkTableName);
+
+  /** alternate constructor for Limelight */
+  public Limelight(String networkTableName) {
+    nt = NetworkTableInstance.getDefault().getTable(networkTableName);
 
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    //botpose is an array of 6 doubles, translation xyz then rotation xyz
-    apriltagmode= nt.getEntry("getpipe").getInteger(-1)==apriltagpipelineindex;
-    retroreflectivemode=nt.getEntry("getpipe").getInteger(-1)==retroreflectivepipelineindex;
-    if(apriltagmode){
-      bp=nt.getEntry("botpose").getDoubleArray(new Double[] {});
+    // botpose is an array of 6 doubles, translation xyz then rotation xyz
+    apriltagmode = nt.getEntry("getpipe").getInteger(-1) == apriltagpipelineindex;
+    retroreflectivemode = nt.getEntry("getpipe").getInteger(-1) == retroreflectivepipelineindex;
+    if (apriltagmode) {
+      bp = nt.getEntry("botpose").getDoubleArray(new Double[] {});
     }
-    tx=nt.getEntry("tx").getDouble(0);
-    ty=nt.getEntry("ty").getDouble(0);
+    tx = nt.getEntry("tx").getDouble(0);
+    ty = nt.getEntry("ty").getDouble(0);
 
     updateWidgets();
   }
-  public Double[] getPose(){
+  /**return bot pose
+   * double array length six
+   * x y z position, x y z rotation
+   */
+  public Double[] getPose() {
     return bp;
   }
-  public double tx(){return tx;}
-  public double ty(){return ty;}
-  public boolean apriltagsAvailable(){
-    return apriltagmode && bp.length>0;
+
+  public double tx() {
+    return tx;
   }
-  public boolean targetsAvailable(){
-    return nt.getEntry("tv").getInteger(-1)==1||(apriltagmode&&bp.length>0);
+
+  public double ty() {
+    return ty;
   }
-  public void setPipeline(int index){
+
+  public boolean apriltagsAvailable() {
+    return apriltagmode && bp.length > 0;
+  }
+
+  public boolean targetsAvailable() {
+    return nt.getEntry("tv").getInteger(-1) == 1 || (apriltagmode && bp.length > 0);
+  }
+
+  public void setPipeline(int index) {
     nt.getEntry("pipeline").setNumber(index);
     nt.getEntry("ledMode").setNumber(0);
   }
-  public boolean apriltagmode(){
+
+  public boolean apriltagmode() {
     return apriltagmode;
   }
-  public boolean retroreflectivemode(){
+
+  public boolean retroreflectivemode() {
     return retroreflectivemode;
   }
-  //not needed
-  private final ShuffleboardTab mainTab = Shuffleboard.getTab("Main");
-  private final GenericPublisher BotPoseWidget=mainTab.add("botpose","").withPosition(2, 1).withSize(4, 1).getEntry();
-  private final GenericPublisher HastargetsWidget=mainTab.add("targets",false).withPosition(1, 1).withSize(1, 1).getEntry();
-  private final GenericPublisher PipelineWidget=mainTab.add("pipelineIndex",0).withPosition(0, 1).withSize(1, 1).getEntry();
 
-  private void updateWidgets(){
+  // not needed
+  private final ShuffleboardTab mainTab = Shuffleboard.getTab("Main");
+  private final GenericPublisher BotPoseWidget = mainTab.add("botpose", "").withPosition(2, 1).withSize(4, 1)
+      .getEntry();
+  private final GenericPublisher HastargetsWidget = mainTab.add("targets", false).withPosition(1, 1).withSize(1, 1)
+      .getEntry();
+  private final GenericPublisher PipelineWidget = mainTab.add("pipelineIndex", 0).withPosition(0, 1).withSize(1, 1)
+      .getEntry();
+
+  private void updateWidgets() {
     HastargetsWidget.setBoolean(targetsAvailable());
     PipelineWidget.setInteger(nt.getEntry("getpipe").getInteger(-1));
-    if(apriltagmode && bp.length>0)
-    BotPoseWidget.setString(bp[0]+","+bp[1]+","+bp[2]+","+bp[3]+","+bp[4]+","+bp[5]);
+    if (apriltagmode && bp.length > 0)
+      BotPoseWidget.setString(bp[0] + "," + bp[1] + "," + bp[2] + "," + bp[3] + "," + bp[4] + "," + bp[5]);
   }
 }
