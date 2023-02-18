0a22761d68351708a94e7437672565570de17fca
maxhv1 <max.hero.factory@gmail.com>
2023-02-18 16:31:49 -0500

    commenting

--- src/main/java/frc/robot/utils/PersistentShufflableDouble.java
+++ src/main/java/frc/robot/utils/PersistentShufflableDouble.java
@@ -7,16 +7,20 @@ public class PersistentShufflableDouble {
     public ShuffleboardTab sdtab;
     private double value;
     private GenericEntry ntEntry;
-    public PersistentShufflableDouble(double initialValue, String tabName, String entryName){
-        sdtab=Shuffleboard.getTab(tabName);
+    public PersistentShufflableDouble(double initialValue, String entryName){
+        sdtab=Shuffleboard.getTab("persistentdoubles");
         value=initialValue;
         sdtab.addPersistent(entryName, value).getEntry();
     }
     public double get(){return value;}
+    /**write the value without shuffleboard*/
     public void set(double Value){value=Value;}
-
+    /**@deprecated @return pull the double from the shufleboard */
     public double subscribe(){return ntEntry.getDouble(value);}
+    /**@deprecated push a double to shuffleboard */
     public void publish(double Value){ntEntry.setDouble(Value);}
+    /**@return pull the double from the shuffleboard and write to value*/
     public double subscribeAndSet(){set(ntEntry.getDouble(value));return value;}
+    /**push the double from the shuffleboard and write to value */
     public void publishAndSet(double Value){ntEntry.setDouble(Value);set(Value);}
 }
