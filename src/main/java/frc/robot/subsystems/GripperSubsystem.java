0663f18e9c960a0268f3759dbb6a3fe78be08b5f
GabrielKim2 <113950931+GabrielKim2@users.noreply.github.com>
2023-02-19 16:50:01 -0500

    Set up framwork for gripper functionality

--- src/main/java/frc/robot/subsystems/GripperSubsystem.java
+++ src/main/java/frc/robot/subsystems/GripperSubsystem.java
@@ -3,13 +3,15 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsControlModule;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class GripperSubsystem extends SubsystemBase {
@@ -21,11 +23,15 @@ public class GripperSubsystem extends SubsystemBase {
     private Compressor compressor = new Compressor(PneumaticsModuleType.CTREPCM);
 
     private double releaseSpeed = 0.1; // wheel speed when releasing a piece
-    private double closeSpeed = -0.1; // wheel speed when capturing a piece
+    private double closeSpeed = -0.1; // wheel speed when holding a piece
+    private double intakeSpeed = 0.2; // wheel speed when capturing a piece
+
+    private double holdingCurrent = 10; // Current spike that determines whether the robot is in possession of a game
+                                        // piece
 
     public GripperSubsystem() {
         doubleSolenoid.set(Value.kOff); // might later want the gripper to default to open/closed?
-        gripperMotor1.setInverted(true); // will need to figure out which motor is inverted
+        gripperMotor1.setInverted(true); // will need to figure out which motor is inverted, if any are inverted at all
     }
 
     public void releaseGripper() {
@@ -44,6 +50,19 @@ public class GripperSubsystem extends SubsystemBase {
         gripperMotor2.set(closeSpeed);
     }
 
+    public void intakeGripper() {
+        // open gripper
+        doubleSolenoid.set(Value.kForward);
+        //
+        gripperMotor1.set(intakeSpeed);
+        gripperMotor1.set(intakeSpeed);
+
+        // Might want this logic in the command instead of the subsystem
+        if (isGamePieceDetected()) {
+            closeGripper();
+        }
+    }
+
     public void reorientGripper() {
         // move gripper to be parrallel with ground. Use gyro?
     }
@@ -52,9 +71,25 @@ public class GripperSubsystem extends SubsystemBase {
         // move gripper to be perpendicular with ground. Use gyro?
     }
 
+    public boolean isGamePieceDetected() {
+        if (gripperMotor1.getOutputCurrent() >= holdingCurrent || gripperMotor2.getOutputCurrent() >= holdingCurrent) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     @Override
     public void periodic() {
 
     }
+
+    public void updateWidgets() {
+        isGamePieceDetectedWidget.setBoolean(isGamePieceDetected());
+    }
+
+    private static final ShuffleboardTab gripperTab = Shuffleboard.getTab("Gripper Tab");
+    private static final GenericEntry isGamePieceDetectedWidget = gripperTab.add("Is Game Piece Detected", false)
+            .getEntry();
 
 }
