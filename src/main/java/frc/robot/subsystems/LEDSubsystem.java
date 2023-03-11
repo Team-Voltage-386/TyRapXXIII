be59d4913607870b4f3be8ac48b4eb9cd7e47749
RyanCofer <rcofer56@gmail.com>
2023-03-11 12:40:23 -0500

    Update LEDSubsystem.java

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -5,33 +5,42 @@ import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.LEDConstants.*;
 
-public class LEDSubsystem extends SubsystemBase{
-
+public class LEDSubsystem extends SubsystemBase
+{
+    //Intializes led length and port
     private static final int LEDPort = kLEDPort;
     private static final int LEDLength = kLEDLength;
     private double BWCycle = 0.0;
 
-    
+    //Sets up the LEDs so we can write values to them
     AddressableLED led = new AddressableLED(LEDPort);
     AddressableLEDBuffer ledBuffer = new AddressableLEDBuffer(LEDLength);
 
-
+    //Sets up all the LED values and starts the LEDs
     public LEDSubsystem() {
         led.setLength(LEDLength);
         led.setData(ledBuffer);
         led.start();
     }
 
+    //Sets the single LED at index to purple
     public void setOnePurple(int index) {
         ledBuffer.setRGB(index, 208, 58, 224);
     }
 
+    //Sets the single LED at index to yellow
     public void setOneYellow(int index) {
         ledBuffer.setRGB(index, 241, 245, 7);
     }
     
+    //Sets the single LED at index to blue
     public void setOneBlue(int index) {
         ledBuffer.setRGB(index, 0, 0, 255);
+    }
+
+    //Turns the single LED at index off
+    public void setOneOff(int index) {
+        ledBuffer.setRGB(index, 0, 0, 0);
     }
 
     // public void alternateBlueYellow() {
@@ -42,24 +51,35 @@ public class LEDSubsystem extends SubsystemBase{
     //     led.setData(ledBuffer);
     // }
     
+    //Sets all of the LEDs on the strip to purple
     public void allPurple() {
         for (int i = 0; i < ledBuffer.getLength(); i++) {
           setOnePurple(i);
         }
     }
     
+    //Turns off all of the LEDs on the strip
     public void allOff() {
         for (int i = 0; i < ledBuffer.getLength(); i++) {
-          ledBuffer.setRGB(i, 0, 0, 0);
+          setOneOff(i);
         }
     }
     
+    //Sets all of the Leds on the strip to Yellow
     public void allYellow() {
         for (int i = 0; i < ledBuffer.getLength(); i++) {
           setOneYellow(i);
         }
     }
 
+    //Alternates all of the LEDs for when the Robot is in cone mode
+    public void AlternatingPurple() {
+        for (int i = 0; i < ledBuffer.getLength(); i++) {
+          setOnePurple(i);
+        }
+    }
+
+    //Huh? (Ask Lucas idk what he was trying to do)
     public void BlueYellow()
     {
         for(int i = 0; i < ledBuffer.getLength(); i++)
