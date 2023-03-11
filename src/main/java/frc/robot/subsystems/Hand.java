7da0e61a1f31bbcf9123166278cdc70367a4afb4
RyanCofer <rcofer56@gmail.com>
2023-03-11 17:52:23 -0500

    Hmm

--- src/main/java/frc/robot/subsystems/Hand.java
+++ src/main/java/frc/robot/subsystems/Hand.java
@@ -70,9 +70,7 @@ public class Hand extends SubsystemBase {
 
     @Override
     public void periodic() {
-        if (handCanRotate) {
-            setHandMotor();
-        }
+        setHandMotor();
         runIntakeMotorState();
         updateWidgets();
     }
@@ -145,24 +143,28 @@ public class Hand extends SubsystemBase {
     }
 
     private void setHandMotor() {
-        if (handPosition == 1 && !getHandLimitSwitch() && getWristAngle() < 75) {
-            HandRotationalMotor.set(ControlMode.PercentOutput, -kRotationSpeed);
-        } else if (handPosition == 0 && getWristAngle() > 0 && !canRetract() && !getHandLimitSwitch()) {
-            HandRotationalMotor.set(ControlMode.PercentOutput, kRotationSpeed);
-        } else if (handPosition == 0 && getWristAngle() < 0 && !canRetract() && !getHandLimitSwitch()) {
-            HandRotationalMotor.set(ControlMode.PercentOutput, -kRotationSpeed);
-        } else if (handPosition == -1 && !getHandLimitSwitch() && getWristAngle() > -75) {
-            HandRotationalMotor.set(ControlMode.PercentOutput, kRotationSpeed);
-            // fix hand positions
-        } else if (handPosition == 1 && getWristAngle() > 78) {
-            HandRotationalMotor.set(ControlMode.PercentOutput, kRotationSpeed);
-        } else if (handPosition == -1 && getWristAngle() < -78) {
-            HandRotationalMotor.set(ControlMode.PercentOutput, -kRotationSpeed);
-            // do nothing
-        } else {
+        if (handCanRotate) {
+            if (handPosition == 1 && !getHandLimitSwitch() && getWristAngle() < 75) {
+                HandRotationalMotor.set(ControlMode.PercentOutput, -kRotationSpeed);
+            } else if (handPosition == 0 && getWristAngle() > 0 && !canRetract() && !getHandLimitSwitch()) {
+                HandRotationalMotor.set(ControlMode.PercentOutput, kRotationSpeed);
+            } else if (handPosition == 0 && getWristAngle() < 0 && !canRetract() && !getHandLimitSwitch()) {
+                HandRotationalMotor.set(ControlMode.PercentOutput, -kRotationSpeed);
+            } else if (handPosition == -1 && !getHandLimitSwitch() && getWristAngle() > -75) {
+                HandRotationalMotor.set(ControlMode.PercentOutput, kRotationSpeed);
+                // fix hand positions
+            } else if (handPosition == 1 && getWristAngle() > 78) {
+                HandRotationalMotor.set(ControlMode.PercentOutput, kRotationSpeed);
+            } else if (handPosition == -1 && getWristAngle() < -78) {
+                HandRotationalMotor.set(ControlMode.PercentOutput, -kRotationSpeed);
+                // do nothing
+            } else {
+                HandRotationalMotor.set(ControlMode.PercentOutput, 0);
+            }
+        }
+        else {
             HandRotationalMotor.set(ControlMode.PercentOutput, 0);
         }
-
     }
 
     public void setRotateHand(boolean isRightBumper) {
