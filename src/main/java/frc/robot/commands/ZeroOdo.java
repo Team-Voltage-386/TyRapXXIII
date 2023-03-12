73dd425360c92cedf0e5da8955c73e8f8c97e090
RyanCofer <rcofer56@gmail.com>
2023-03-12 10:50:33 -0400

    Create ZeroOdo.java

---
+++ src/main/java/frc/robot/commands/ZeroOdo.java
@@ -0,0 +1,33 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
+
+public class ZeroOdo extends CommandBase {
+
+    boolean done = false;
+    double x;
+    double y;
+    double a;
+    Drivetrain dt;
+
+    public ZeroOdo(double X, double Y, double A, Drivetrain DT) {
+        x =X;
+        y = Y;
+        a = A;
+        dt = DT;
+    }
+
+    @Override
+    public void initialize() {
+        dt.xPos = x;
+        dt.yPos = y;
+        dt.resetFO();
+        done = true;
+    }
+
+    public boolean isFinished() {
+        return done;
+    }
+    
+}
