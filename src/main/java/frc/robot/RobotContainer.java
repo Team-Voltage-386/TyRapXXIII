c8cc4eb9ec167f80e7771b6d3cff8760d68ac88a
RyanCofer <rcofer56@gmail.com>
2023-03-13 20:33:16 -0400

    latest

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -115,7 +115,7 @@ public class RobotContainer {
         new ArmDo(m_Arm, kfseqConeStowToConeHigh),
         new HandTasks(false, handIntakeStates.doNothing, HandControls),
         new ParallelCommandGroup(new ArmDo(m_Arm, kfseqConeHightoCubeStow),
-            new Drive(-3, 0, 0, m_driveTrain)));
+        new Drive(3.5, 0, 180, m_driveTrain)));
     // public final Command test3 = new SequentialCommandGroup(
     //     new ManualFeedOdometry(m_driveTrain, 0, 0, (AllianceData.resetOrientationOffset + 180) % 360),
     //     new HandTasks(true, handIntakeStates.stow, HandControls),
