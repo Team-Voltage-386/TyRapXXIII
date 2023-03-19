cbc60b53ced9e0f607de49566b5fb4a931051b48
LucasCodee <lucascornejo222@gmail.com>
2023-03-19 12:54:30 -0400

    new balance cmd

--- src/main/java/frc/robot/commands/Autonomous/Balance.java
+++ src/main/java/frc/robot/commands/Autonomous/Balance.java
@@ -1,32 +1,25 @@
 package frc.robot.commands.Autonomous;
 
-import com.ctre.phoenix.sensors.Pigeon2;
-import static frc.robot.Constants.DriveConstants.*;
-
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.Tracer;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
-import frc.robot.utils.PID;
-import frc.robot.utils.PIDShufflable;
 
 public class Balance extends CommandBase {
     //init vars and methods
-    private double balanceTarget = 2;
+    private double balanceTarget = 4;
     private final Drivetrain dt;
     private Timer time = new Timer();
-    private final PIDShufflable pid = new PIDShufflable(0.03, 0, 1.5, "BalanceInfo");
-    //controls if the robot is Xlocked or not
-    private boolean XLOCK = false;
+    //controls if the robot is HasBeenBalanceded or not
+    private boolean HasBeenBalanced = false;
     //shuffleboard stuff
     public static ShuffleboardTab mainTab = Shuffleboard.getTab("BalanceInfo");
     private static final GenericEntry xPosWidget = mainTab.add("X", 0).withPosition(0, 0).withSize(1, 1).getEntry();
     private static final GenericEntry yPosWidget = mainTab.add("Y", 0).withPosition(1, 0).withSize(1, 1).getEntry();
     private static final GenericEntry pitchWid = mainTab.add("Pitch",0).withPosition(0, 1).withSize(1,1).getEntry();
-    private static final GenericEntry XLOCKWid = mainTab.add("XLOCK",false).withPosition(1,1).withSize(1,1).getEntry();
+    private static final GenericEntry HasBeenBalancedWid = mainTab.add("HasBeenBalanced",false).withPosition(1,1).withSize(1,1).getEntry();
     private static final GenericEntry Timer = mainTab.add("Timer",0).withPosition(0,2).withSize(1,1).getEntry();
 
     public Balance(Drivetrain DT) {
@@ -48,13 +41,13 @@ public class Balance extends CommandBase {
         xPosWidget.setDouble(dt.xPos);
         yPosWidget.setDouble(dt.yPos);
         pitchWid.setDouble(dt.ypr[2]);
-        XLOCKWid.setBoolean(XLOCK);
+        HasBeenBalancedWid.setBoolean(HasBeenBalanced);
         Timer.setDouble(time.get());
 
-        //balancing if not xlock
-        if(!XLOCK)
-        dt.xDriveTarget = -pid.calc(0 - dt.ypr[2]);
-        else dt.xDriveTarget = 0;
+        //balancing if not HasBeenBalanced
+        if(!HasBeenBalanced)
+        dt.xDriveTarget = 0.1;
+        else dt.xDriveTarget = -0.05;
 
         //BALANCE SYSTEM
         boolean isBalanced = false;
@@ -69,13 +62,13 @@ public class Balance extends CommandBase {
             isBalanced = false;
         }
 
-        if(time.get() > 0.1) XLOCK = true;
-        else XLOCK = false;
+        if(time.get() > 0.1) HasBeenBalanced = true;
+        else HasBeenBalanced = false;
     }
 
     @Override
     public boolean isFinished() {
-        return false;
+        return HasBeenBalanced && time.get() > 0.4;
     }
 
     @Override
