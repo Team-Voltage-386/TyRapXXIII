8c69ad49e8bd8e3347ea84dd2861ed5492ace7cc
LucasCodee <lucascornejo222@gmail.com>
2023-04-03 19:14:55 -0400

    a

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,6 +18,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -101,10 +102,9 @@ public class RobotContainer {
         PathPlannerTrajectory TestPath = PathPlanner.loadPath("Test Path 1", new PathConstraints(4, 3));
 
         public final class AutoRoutines {
-            public final Command TestPP(
-                
+            public final Command TestPP = new SequentialCommandGroup(
+                new FollowTrajectoryCommand(TestPath, true)
             );
-
         }
 
     /**
