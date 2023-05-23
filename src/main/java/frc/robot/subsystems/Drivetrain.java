06e5232f3bbea6e7386e9d98588718793669e9ca
LucasCodee <lucascornejo222@gmail.com>
2023-05-23 18:27:12 -0400

    a

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,6 +4,8 @@ import static frc.robot.Constants.DriveConstants.*;
 
 import com.ctre.phoenix.sensors.Pigeon2;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.Timer;
@@ -33,6 +35,7 @@ public class Drivetrain extends SubsystemBase {
     private Timer odometryTimer = new Timer();
     private long odoTimerLast = 0;
     public boolean doFieldOrientation = true;
+    public Pose2d pose;
 
     public SwerveModule[] modules = { RightFront, RightRear, LeftRear, LeftFront };
 
@@ -171,12 +174,34 @@ public class Drivetrain extends SubsystemBase {
             yPos += yAdd;
 
             speed = Math.sqrt(Math.pow(xSpeed, 2) + Math.pow(ySpeed, 2));
-
         }
+        pose = new Pose2d(xPos, yPos, getHeadingRotation2d());
     }
 
     public double distanceTo(double x, double y) {
         return Math.sqrt(Math.pow(x - xPos, 2) + Math.pow(y - yPos, 2));
+    }
+
+    //*zeros heading */
+    public void zeroHeading() {
+        IMU.setYaw(0);
+    }
+    
+    //*get heading of robot in degrees*/
+    public double getHeading() {
+        return Math.IEEEremainder((IMU.getYaw()), 360);
+    }
+
+    public Rotation2d getHeadingRotation2d() {
+        return Rotation2d.fromDegrees(getHeading());
+    }
+
+    public Pose2d getPose() {
+        return pose;
+    }
+
+    public void resetOdometry(Pose2d p) {
+        pose = p;
     }
 
     public double getHeadingError(double h) {
